92e724b9f1913d04604b146c58a04e404f4d8e8e
Fabio Velloso <git-tower@fabiovelloso.com.br>
2017-07-14 00:42:29 -0300

    Initial commit

---
+++ src/main/java/com/flvs/Teste.java
@@ -0,0 +1,10 @@
+package com.flvs;
+
+public class Teste {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Teste");
+    }
+}
